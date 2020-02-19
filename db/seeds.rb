eca0f9727f507beadec4feacd3eb49b360fc28fe
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-02-19 15:28:50 +0100

    MSG

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'italian',
+    phone_number:        "5",
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "4",
+  },
+  {
+    name:         'Pizza East2',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "4",
+  },
+  {
+    name:         'Pizza East3',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "4",
+  },
+  {
+    name:         'Pizza East4',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "4",
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
